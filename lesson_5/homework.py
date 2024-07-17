7e0a19a0b03b4035661dffb791d898abd56b5710
tamiralkateeb <137404017+tamiralkateeb@users.noreply.github.com>
2024-07-16 20:36:28 -0400

    home work is done lesson5

--- lesson_5/homework.py
+++ lesson_5/homework.py
@@ -4,61 +4,93 @@
 
 # Challenge 1
 # Make a number Positive
-#
+# Challenge 1
+# Make a number Positive
+
 # Create a variable called my_number and set it to any integer value.
+my_number = -3  # Example input, you can change this to any integer
+
 # Write code to make the number positive if it's negative, and keep it
 # as is if it's already positive or zero.
-#
-# Example:
-# Input: -3 => Output: 3
-# Input: 5 => Output: 5
+if my_number < 0:
+    my_number = -my_number
 
+print(my_number)  # Output: 3 if input is -3, 5 if input is 5
 
 # ---------------------------------------------------------------------
 
 # Challenge 2
 # BinGo!
-#
-# If the number is divisible of 3, print “Bin”
-# If the number is divisible of 7, print “Go”
-# For numbers which are divisible of 3 and 7, print “BinGo”
+# Challenge 2
+# BinGo!
+
+# If the number is divisible by 3, print “Bin”
+# If the number is divisible by 7, print “Go”
+# For numbers which are divisible by 3 and 7, print “BinGo”
 # Otherwise, print the original number: “{number} is just a number”
 
+number = 21  # Example input, you can change this to any integer
+
+if number % 3 == 0 and number % 7 == 0:
+    print("BinGo")
+elif number % 3 == 0:
+    print("Bin")
+elif number % 7 == 0:
+    print("Go")
+else:
+    print(f"{number} is just a number")
 
 # ---------------------------------------------------------------------
 
 # Challenge 3
+# Challenge 3
 # Find the middle number
-#
+
 # Given three different numbers x, y, and z, find the number that is neither
 # the smallest nor the largest and print it.
-#
-# Example:
-# x = 1, y = 5, z = 3 => Output: 3
 
+x, y, z = 1, 5, 3  # Example input, you can change this to any three different integers
+
+if (y < x < z) or (z < x < y):
+    middle = x
+elif (x < y < z) or (z < y < x):
+    middle = y
+else:
+    middle = z
+
+print(middle)  # Output: 3 if inputs are x=1, y=5, z=3
 
 # ---------------------------------------------------------------------
 
 # Challenge 4
+# Challenge 4
 # Palindrome Numbers
-#
-# Ask a user to input a number.
-# Write a program to check if the given number is a palindrome.
-# It should print True if the number is a palindrome and False if it is not.
-#
-# Palindrome number: 121, 898
 
+# Ask a user to input a number.
+number = input("Enter a number: ")
+
+# Write a program to check if the given number is a palindrome.
+is_palindrome = number == number[::-1]
+
+# It should print True if the number is a palindrome and False if it is not.
+print(is_palindrome)  # Example: True if input is 121, False if input is 123
 
 # ---------------------------------------------------------------------
 
 # Challenge 5
 # Reverse a string
-#
-# You're part of a team working on analyzing customer reviews for a new video game.
-# Due to a software glitch, some reviews have been recorded in reverse with punctuation
-# at the beginning instead of the end. Your task is to correct these reviews so that they
-# are in the correct order and the punctuation is appropriately placed at the end of the
-# sentence or word.
-#
+# Challenge 5
+# Reverse a string
+
 # Example: "tcefreP!" -> Perfect!
 
+review = "tcefreP!"  # Example input, you can change this to any string
+# Strip punctuation from the beginning
+punctuation = ''
+while not review[0].isalpha():
+    punctuation += review[0]
+    review = review[1:]
+
+# Reverse the string
+corrected_review = review[::-1] + punctuation
+print(corrected_review)  # Output: "Perfect!"
